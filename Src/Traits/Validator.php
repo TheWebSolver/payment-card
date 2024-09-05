ae3615fb3cf8199aaa6f7d5955bedc9b891be0da
Shesh Ghimire <sheshgh@outlook.com>
2024-09-05 14:45:01 +0545

    Added Validator test.

--- Src/Traits/Validator.php
+++ Src/Traits/Validator.php
@@ -9,6 +9,7 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\PaymentCard\Traits;
 
+use InvalidArgumentException;
 use TheWebSolver\Codegarage\PaymentCard\Asserter;
 
 /**
@@ -16,43 +17,25 @@ use TheWebSolver\Codegarage\PaymentCard\Asserter;
  * `TheWebSolver\Codegarage\PaymentCard\CardInterface`
  */
 trait Validator {
-
 	abstract public function getLength(): array;
 	abstract public function getPattern(): array;
 	abstract public function getCode(): array;
 
 	public function isNumberValid( mixed $subject, bool $withLuhnAlgorithm = true ): bool {
-		if ( ! is_scalar( $subject ) || ! $subject = Asserter::normalize( (string) $subject ) ) {
-			return false;
-		}
-
-		if ( ! $this->matchesLengths( $subject ) ) {
-			return false;
-		}
-
-		if ( $withLuhnAlgorithm && ! $this->matchesLuhnAlgorithm( $subject ) ) {
-			return false;
-		}
-
-		return $this->matchesPatterns( $subject );
+		return $this->matchesType( $subject )
+			&& $this->matchesLengths( $subject )
+			&& $this->matchesLuhnAlgorithm( $subject, shouldRun: $withLuhnAlgorithm )
+			&& $this->matchesPatterns( $subject );
 	}
 
 	public function isCodeValid( mixed $subject ): bool {
-		return is_scalar( $subject ) && strlen( (string) $subject ) === $this->getCode()[1];
+		return ( is_string( $subject ) || is_int( $subject ) )
+			&& strlen( (string) $subject ) === ( $this->getCode()[1] ?? false );
 	}
 
 	protected function matchesLengths( string $subject ): bool {
-		$count = strlen( $subject );
-
 		foreach ( $this->getLength() as $length ) {
-			if ( is_array( $length ) ) {
-				[ $min, $max ] = $length;
-				$isValid       = $count >= (int) $min && $count <= (int) $max;
-			} else {
-				$isValid = (int) $length === strlen( $subject );
-			}
-
-			if ( $isValid ) {
+			if ( self::matchesCardNumberSize( $length, value: $subject ) ) {
 				return true;
 			}
 		}
@@ -60,15 +43,15 @@ trait Validator {
 		return false;
 	}
 
-	protected function matchesLuhnAlgorithm( string $subject ): bool {
-		return class_exists( '\\TheWebSolver\\Codegarage\\LuhnAlgorithm' )
+	protected function matchesLuhnAlgorithm( string $subject, bool $shouldRun ): bool {
+		return $shouldRun && class_exists( '\\TheWebSolver\\Codegarage\\LuhnAlgorithm' )
 			? \TheWebSolver\Codegarage\LuhnAlgorithm::validate( value: $subject )
 			: true;
 	}
 
 	protected function matchesPatterns( string $subject ): bool {
 		foreach ( $this->getPattern() as $pattern ) {
-			if ( $this->matchesPattern( $subject, $pattern ) ) {
+			if ( $this->matchesPattern( $pattern, $subject ) ) {
 				return true;
 			}
 		}
@@ -76,29 +59,45 @@ trait Validator {
 		return false;
 	}
 
-	/**
-	 * @param string|int|string[]|int[] $pattern
-	 */ // phpcs:ignore Squiz.Commenting.FunctionComment.ParamNameNoMatch
-	protected function matchesPattern( string $subject, string|int|array $pattern ): bool {
+	/** @param string|int|string[]|int[] $pattern */
+	private function matchesPattern( string|int|array $pattern, string $subject ): bool {
 		return is_array( $pattern )
 		? $this->matchesBetween( $subject, ...$pattern )
 		: $this->matchesExact( $subject, $pattern );
 	}
 
-	protected function matchesBetween( string $subject, string|int $min, string|int $max ): bool {
-		$count = self::subjectPart( $subject, (string) $min, (string) $max );
+	private function matchesBetween( string $subject, string|int $min, string|int $max ): bool {
+		$count = $this->subjectPart( $subject, (string) $min, (string) $max );
 
 		return $count >= (int) $min && $count <= (int) $max;
 	}
 
-	protected function matchesExact( string $subject, string|int $pattern ): bool {
+	private function matchesExact( string $subject, string|int $pattern ): bool {
 		$CardPart    = substr( $subject, offset: 0, length: strlen( (string) $pattern ) );
 		$patternPart = substr( (string) $pattern, offset: 0, length: strlen( $subject ) );
 
 		return $patternPart === $CardPart;
 	}
 
-	private static function subjectPart( string $subject, string $min, string $max ): int {
+	private function matchesType( mixed &$subject ): bool {
+		return is_scalar( $subject ) && ( $subject = Asserter::normalize( (string) $subject ) );
+	}
+
+	private function matchesCardNumberSize( mixed $length, string $value ): bool {
+		$count = strlen( $value );
+
+		try {
+			$size = Asserter::assertHasSize( $length );
+		} catch ( InvalidArgumentException ) {
+			$size = 0;
+		}
+
+		return ! is_array( $size )
+			? $count === $size
+			: ( $size[0] ?? 0 ) && ( $size[1] ?? 0 ) && ( $count >= $size[0] && $count <= $size[1] );
+	}
+
+	private function subjectPart( string $subject, string $min, string $max ): int {
 		$maxLength = max( strlen( $min ), strlen( $max ) );
 
 		return (int) substr( $subject, offset: 0, length: $maxLength );
---
+++ Tests/ValidatorTest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Validator Test.
+ *
+ * @package TheWebSolver\Codegarage\Test
+ */
+
+declare( strict_types = 1 );
+
+namespace TheWebSolver\Codegarage;
+
+use PHPUnit\Framework\TestCase;
+use TheWebSolver\Codegarage\PaymentCard\Traits\Validator;
+
+class ValidatorTest extends TestCase {
+	/**
+	 * @param mixed[] $code
+	 * @dataProvider provideCodes
+	 */
+	public function testCodeIsValid( array $code, mixed $subject, bool $expected ): void {
+		$class = new class( $code ) {
+			use Validator;
+
+			/** @param mixed[] $code */
+			public function __construct( private readonly array $code ) {}
+
+			/** @return (string|int|(string|int)[])[] */
+			public function getLength(): array {
+				return array();
+			}
+
+			/** @return (string|int|(string|int)[])[] */
+			public function getPattern(): array {
+				return array();
+			}
+
+			/** @return mixed[] */
+			public function getCode(): array {
+				return $this->code;
+			}
+		};
+
+		$this->assertSame( $expected, $class->isCodeValid( $subject ) );
+	}
+
+	/** @return mixed[] */
+	public function provideCodes(): array {
+		return array(
+			array( array( 'Test', 1 ), 5, true ),
+			array( array( 'Test', 2 ), '55', true ),
+			array( array( 'Test', 1 ), true, false ),
+			array( array( 'Test', 4 ), 798, false ),
+			array( array( 'Test', 4 ), '7989', true ),
+			array( array( 'Test' ), '7989', false ),
+			array( array( 'Test', 3 ), '989', true ),
+		);
+	}
+
+	/**
+	 * @param (string|int|(string|int)[])[] $length
+	 * @param (string|int|(string|int)[])[] $pattern
+	 * @dataProvider provideNumbers
+	 */
+	public function testNumberIsValid(
+		array $length,
+		array $pattern,
+		mixed $subject,
+		bool $status,
+		bool $withLuhnAlgorithm = true
+	): void {
+		$class = new class( $length, $pattern ) {
+			use Validator;
+
+			/**
+			 * @param (string|int|(string|int)[])[] $length
+			 * @param (string|int|(string|int)[])[] $pattern
+			 */
+			public function __construct( private array $length, private array $pattern ) {}
+
+			/** @return (string|int|(string|int)[])[] */
+			public function getLength(): array {
+				return $this->length;
+			}
+
+			/** @return (string|int|(string|int)[])[] */
+			public function getPattern(): array {
+				return $this->pattern;
+			}
+
+			/** @return mixed[] */
+			public function getCode(): array {
+				return array();
+			}
+		};
+
+		$this->assertSame(
+			expected: $status,
+			actual: $class->isNumberValid( $subject, $withLuhnAlgorithm )
+		);
+	}
+
+	/** @return mixed[] */
+	public function provideNumbers(): array {
+		return array(
+			array( array( 12, 14 ), array( 432 ), 432187659876, true ),
+			array( array( 12, 14 ), array( 432 ), fn() => 432187659876, false ),
+			array( array( 12, 14 ), array( 432 ), '43218765987699', true ),
+			array( array( 12, 14 ), array( 432 ), 43218765987, false ),
+			array( array( array( 13, 15 ) ), array( 432 ), 43218765987, false ),
+			array( array( array( 13, 15 ) ), array( 432 ), 43218765987699, true ),
+			array( array( array( 13, 15 ) ), array( 433 ), 43218765987699, false ),
+			array( array( array( 15, 13 ) ), array( 432 ), 43218765987699, false ),
+			array( array( array( 15 ) ), array( 432 ), 43218765987699, false ),
+			array( array( 6, array( 9, 14 ), 17 ), array( 432 ), 43218765987699, true ),
+			array( array( 6, array( 9, 13 ), 17 ), array( 432 ), 43218765987699, false ),
+			array( array( 6, array( 9, 14 ), 17 ), array( 432, array( 55, 59 ), 71 ), 56218765987699, true ),
+			array( array( 6, array( 9, 14 ), 17 ), array( 432, array( 55, 59 ), 71 ), 71218765987699, true ),
+
+		);
+	}
+}
