14cc8912a3418f6cfb5b81c10e3085c77961dd76
Shesh Ghimire <sheshgh@outlook.com>
2025-12-12 14:48:56 +0545

    new test: test file for `CodeTransformer`.

--- Src/Transformer/CodeTransformer.php
+++ Src/Transformer/CodeTransformer.php
@@ -5,18 +5,19 @@ namespace TheWebSolver\Codegarage\PaymentCard\Transformer;
 
 use DOMElement;
 use TheWebSolver\Codegarage\Scraper\Error\ScraperError;
+use TheWebSolver\Codegarage\Scraper\Error\InvalidSource;
 use TheWebSolver\Codegarage\Scraper\Interfaces\Transformer;
 
 /** @template-implements Transformer<object,array{name:string,size:int}> */
 class CodeTransformer implements Transformer {
 	/** @placeholder `%s:` Card's Code property names. */
-	final public const PATTERN_DEFINITION = '(?(DEFINE)(?<properties>[%s]+)(?<separator>[\:\" ]+?)(?<names>[A-Z]{3})(?<sizes>[\d]{1}))';
+	final public const PATTERN_DEFINITION = '(?(DEFINE)(?<properties>(%s))(?<separator>[\:\" ]+)(?<names>[A-Z]{3})(?<sizes>[\d]{1}))';
 
 	final public const PROPERTIES = [ 'name', 'size' ];
 
 	final public const INVALID_ELEMENT = 'Invalid element type provided to transform Card\'s Code.';
-	/** @placeholder: `1:` Card's Code property names, `2:` The given string. */
-	final public const INVALID_PROPERTIES = 'Card\'s Code must have values associated with properties "%1$s". "%2$s" given.';
+	/** @placeholder: `%s:` Missing property name. */
+	final public const MISSING_PROPERTY = 'Missing required "%s" property in Card\'s Code transformation.';
 
 	public static function getRegexPattern(): string {
 		$define = sprintf( self::PATTERN_DEFINITION, implode( '|', self::PROPERTIES ) );
@@ -26,43 +27,46 @@ class CodeTransformer implements Transformer {
 
 	/**
 	 * @return array{name:string,size:int}
-	 * @throws ScraperError When Card's Code source is invalid.
+	 * @throws ScraperError When Card's Code source regex match fails or property is missing.
 	 */
 	public static function transformCode( string $source ): array {
 		$details = preg_match_all( self::getRegexPattern(), $source, $matches, PREG_SET_ORDER )
 			? array_reduce( $matches, self::reduceToProperties( ... ), initial: [] )
 			: null;
 
-		return $details ?: ScraperError::patternMismatch( "Card's Code", self::getRegexPattern(), $source );
+		return match ( true ) {
+			is_null( $details )         => throw ScraperError::patternMismatch( "Card's Code", self::getRegexPattern(), $source ),
+			! isset( $details['name'] ) => throw ScraperError::trigger( self::MISSING_PROPERTY, 'name' ),
+			! isset( $details['size'] ) => throw ScraperError::trigger( self::MISSING_PROPERTY, 'size' ),
+			default                     => $details
+		};
 	}
 
 	public function transform( string|array|DOMElement $element, object $scope ): mixed {
-		$value = match ( true ) {
+		$source = match ( true ) {
 			$element instanceof DOMElement         => $element->textContent,
 			is_string( $element )                  => $element,
 			is_string( $element['value'] ?? null ) => $element['value'],
-			default                                => throw ScraperError::trigger( self::INVALID_ELEMENT )
+			default                                => throw new InvalidSource( self::INVALID_ELEMENT )
 		};
 
-		return $this->transformCode( $value );
+		return $this->transformCode( $source );
 	}
 
 	/**
 	 * @param array{name?:string,size?:int} $properties
-	 * @param array<string>                 $property
-	 * @return array{name:string,size:int}
+	 * @param string[]                      $property
+	 * @return array{name?:string,size?:int}
 	 * @throws ScraperError When Card's Code properties are invalid.
 	 */
 	private static function reduceToProperties( array $properties, array $property ): array {
-		$propertyName = $property['name'] ?? null;
+		$propertyName = $property['name'];
 
-		match ( $propertyName ) {
-			'name'  => $properties['name'] = $property['value'],
-			'size'  => $properties['size'] = NumericTransformer::maybeToDigit( $property['value'] ),
-			default => throw ScraperError::trigger( self::INVALID_PROPERTIES, implode( '", "', self::PROPERTIES ), $property[0] ),
-		};
-
-		assert( isset( $properties['name'], $properties['size'] ) );
+		if ( 'name' === $propertyName ) {
+			$properties['name'] = $property['value'];
+		} elseif ( 'size' === $propertyName ) {
+			$properties['size'] = NumericTransformer::maybeToDigit( $property['value'] );
+		}
 
 		return $properties;
 	}
---
+++ Tests/Transformer/CodeTransformerTest.php
@@ -0,0 +1,110 @@
+<?php
+declare( strict_types = 1 );
+
+namespace TheWebSolver\Codegarage\Test\Transformer;
+
+use stdClass;
+use DOMElement;
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\DataProvider;
+use TheWebSolver\Codegarage\Scraper\Error\ScraperError;
+use TheWebSolver\Codegarage\Scraper\Error\InvalidSource;
+use TheWebSolver\Codegarage\PaymentCard\Transformer\CodeTransformer;
+
+class CodeTransformerTest extends TestCase {
+	#[Test]
+	public function itVerifiesRegexPattern(): void {
+		$define   = sprintf( CodeTransformer::PATTERN_DEFINITION, 'name|size' );
+		$expected = "/{$define}(?<name>(?&properties))(?&separator)(?<value>(?&names)|(?&sizes))/";
+
+		$this->assertSame( $expected, CodeTransformer::getRegexPattern() );
+	}
+
+	/**
+	 * @param string|mixed[]|DOMElement $element
+	 * @param string|mixed[]            $expected
+	 */
+	#[Test]
+	#[DataProvider( 'provideCodeElements' )]
+	public function itTransformsGivenElement( string|array|DOMElement $element, string|array $expected, string $throwable = '' ): void {
+		if ( is_string( $expected ) ) {
+			$this->expectException( $throwable );
+			$this->expectExceptionMessage( $expected );
+		}
+
+		$this->assertSame( $expected, ( new CodeTransformer() )->transform( $element, new stdClass() ) );
+	}
+
+	public static function provideCodeElements(): array {
+		$dom = new DOMDocument();
+		$dom->loadHTML( '<code>name:"CVC" size:3</code>' );
+
+		return [
+			[
+				'name:"CVC" size:3',
+				[
+					'name' => 'CVC',
+					'size' => 3,
+				],
+			],
+			[
+				'{
+           name: "CVC",
+           size: 3
+        }',
+				[
+					'name' => 'CVC',
+					'size' => 3,
+				],
+			],
+			[
+				[ 'value' => 'name:"CVV" size:4' ],
+				[
+					'name' => 'CVV',
+					'size' => 4,
+				],
+			],
+			[
+				[
+					'value' => '{
+            name: "CVC",
+            size: 3
+          }',
+				],
+				[
+					'name' => 'CVC',
+					'size' => 3,
+				],
+			],
+			[
+				$dom->getElementsByTagName( 'code' )->item( 0 ),
+				[
+					'name' => 'CVC',
+					'size' => 3,
+				],
+			],
+			[
+				[ 'key-must-be-named-"value"' => 'name:"CVV" size:4' ],
+				CodeTransformer::INVALID_ELEMENT,
+				InvalidSource::class,
+			],
+			[
+				'{names:"CVV", sizes:3}',
+				'Cannot match pattern to given subject: "{names:"CVV", sizes:3}"',
+				ScraperError::class,
+			],
+			[
+				'{names:"CVV", size:3}',
+				sprintf( CodeTransformer::MISSING_PROPERTY, 'name' ),
+				ScraperError::class,
+			],
+			[
+				'{name:"CVV", sizes:3}',
+				sprintf( CodeTransformer::MISSING_PROPERTY, 'size' ),
+				ScraperError::class,
+			],
+		];
+	}
+}
