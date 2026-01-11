9a41799a572d9d2d1a49e1b8a50a5048b6cd81a3
Shesh Ghimire <sheshgh@outlook.com>
2026-01-11 13:20:31 +0545

    move and remove: classname and traits previously renamed for autoloading.

--- Tests/Resource/NapasCard.php
+++ Tests/Resource/NapasCard.php
@@ -4,6 +4,6 @@ declare( strict_types = 1 );
 // phpcs:ignore PHPCompatibility.Keywords.ForbiddenNamesAsDeclared.resourceFound
 namespace TheWebSolver\Codegarage\Test\Resource;
 
-use TheWebSolver\Codegarage\PaymentCard\CardType;
+use TheWebSolver\Codegarage\PaymentCard\PaymentCardType;
 
-class NapasCard extends CardType {}
+class NapasCard extends PaymentCardType {}
