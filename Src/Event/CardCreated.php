9c2281c8083c954460c8564975dc847ec1a8700a
Shesh Ghimire <sheshgh@outlook.com>
2026-01-19 12:13:22 +0545

    security: `CardCreated::$card` property as `CardType` instead of object.

--- Src/Event/CardCreated.php
+++ Src/Event/CardCreated.php
@@ -3,11 +3,13 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\PaymentCard\Event;
 
-/** @template TCardType of object */
+use TheWebSolver\Codegarage\PaymentCard\Interfaces\CardType;
+
+/** @template TCardType of CardType */
 final readonly class CardCreated {
 	/** @param TCardType|null $card The card instance created. null if is not a creatable card or other custom logic. */
 	public function __construct(
-		public ?object $card,
+		public ?CardType $card,
 		public string|int $payloadIndex,
 		public mixed $payloadValue,
 		public bool $isCreatableCard
