49c89a0fe776257f7e7210eaa93fba9c38318298
Shesh Ghimire <sheshgh@outlook.com>
2026-01-30 20:09:57 +0545

    update: `ResolvedAction::with()` method name.

--- Src/Interfaces/ResolvedAction.php
+++ Src/Interfaces/ResolvedAction.php
@@ -9,10 +9,10 @@ interface ResolvedAction {
 	/**
 	 * Sets card resolver that resolves card number.
 	 */
-	public function resolvedWith( ResolvesCard $resolver ): self;
+	public function with( ResolvesCard $resolver ): self;
 
 	/**
-	 * Handles resolved cord type.
+	 * Handles resolved card type.
 	 */
 	public function handle( CardResolved $event ): void;
 }
