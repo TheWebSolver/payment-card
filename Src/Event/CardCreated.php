7a4d63ace7177c3eeff218c2d32f0de7c1078fdc
Shesh Ghimire <sheshgh@outlook.com>
2026-01-30 20:00:31 +0545

    add: propagation setter and getter methods after card is created.

--- Src/Event/CardCreated.php
+++ Src/Event/CardCreated.php
@@ -7,15 +7,17 @@ use LogicException;
 use TheWebSolver\Codegarage\PaymentCard\Interfaces\CardType;
 
 /** @template TCardType of CardType */
-final readonly class CardCreated {
+final class CardCreated {
 	public const NOT = 'Card not instantiated using given payload value. Verify using "' . __CLASS__ . '::$isCreatableCard" if it a creatable card?';
+
+	private bool $stopPropagation = false;
 
 	/** @param TCardType|null $card The card instance created. null if is not a creatable card. */
 	public function __construct(
-		private ?CardType $card,
-		public string|int $payloadIndex,
-		public mixed $payloadValue,
-		public bool $isCreatableCard
+		private readonly ?CardType $card,
+		public readonly string|int $payloadIndex,
+		public readonly mixed $payloadValue,
+		public readonly bool $isCreatableCard
 	) {}
 
 	/**
@@ -26,5 +28,13 @@ final readonly class CardCreated {
 	 */
 	public function card(): CardType {
 		return $this->card ?? throw new LogicException( self::NOT );
+	}
+
+	public function stopPropagation( bool $stop = true ): void {
+		$this->stopPropagation = $stop;
+	}
+
+	public function shouldStopPropagation(): bool {
+		return $this->stopPropagation;
 	}
 }
